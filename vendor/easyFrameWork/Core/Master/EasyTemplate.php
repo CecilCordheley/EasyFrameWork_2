784b458efd463554193584c9eeac5522374ddd83
CecilCordheley <35634636+CecilCordheley@users.noreply.github.com>
2024-04-13 12:36:53 +0200

    Add files via upload
    Change les variables GET

--- vendor/easyFrameWork/Core/Master/EasyTemplate.php
+++ vendor/easyFrameWork/Core/Master/EasyTemplate.php
@@ -14,22 +14,18 @@ class EasyTemplate
 
     public function __construct(array $config, ResourceManager $resourceManager)
     {
-        echo "<!--Initilize Template-->";
         $this->config = $config;
         $this->resourceManager = $resourceManager;
         $this->loadContent();
     }
-
+    public function getRessourceManager():ResourceManager{
+        return $this->resourceManager;
+    }
     private function loadContent()
     {
-        if (file_exists($this->config['templateDirectory'] . '/' . $this->config['masterPage'])) {
-            $this->content = file_get_contents($this->config['templateDirectory'] . '/' . $this->config['masterPage']);
-            echo "<!--Master Template Loaded-->";
-        } else
-            echo "<!--Error Master Template Can't be Load-->";
+        $this->content = file_get_contents($this->config['templateDirectory'] . '/' . $this->config['masterPage']);
     }
-    public function changeMaster($newMaster)
-    {
+    public function changeMaster($newMaster){
         $this->content = file_get_contents($this->config['templateDirectory'] . '/' . $newMaster);
     }
     public function setVariables(array $variables)
@@ -43,21 +39,16 @@ class EasyTemplate
     }
     public function render(array $customReplacements = [])
     {
-        echo "<!--Render Template-->";
         $this->renderStylesheets();
         $this->renderScripts();
-        $this->renderMicroData();
-        $this->replaceVariables();
-        $this->replaceCondition();
         foreach ($this->loops as $key => $loop) {
             $this->replaceLoop($key, $loop);
         }
-
-
+        $this->replaceVariables();
         $this->replaceRootURL();
         $this->replaceImageURL();
         $this->replaceSessionVariables();
-
+        $this->replaceGetVariable();
         // Exécute les méthodes de substitution personnalisées
         // var_dump($customReplacements);
         foreach ($customReplacements as $customReplacement) {
@@ -68,20 +59,16 @@ class EasyTemplate
             }
         }
         $this->clear();
-        //  var_dump($this->content);
         echo $this->content;
     }
-    private function clear()
-    {
-        $this->content = preg_replace("/\{var:(.*?)\}/i", "", $this->content);
+    private function clear(){
+        $this->content=preg_replace("/\{var:(.*?)\}/i","",$this->content);
     }
     public function addScript($scriptPath)
     {
         $this->resourceManager->addScript($scriptPath);
     }
-    public function renderMicroData(){
-        $this->resourceManager->renderMicroData($this->content);
-    }
+
     public function addStylesheet($stylesheetPath)
     {
         $this->resourceManager->addStylesheet($stylesheetPath);
@@ -100,66 +87,46 @@ class EasyTemplate
     {
         $this->loops[$key] = $a;
     }
-    public function getRessourceManager():ResourceManager{
-        return $this->resourceManager;
-    }
-    private function replaceCondition()
-    {
-        $pattern = "/\{\:IF (.*?)(=|!|>|<)(.*?)\}(.*?)(?:\{\:ELSE\:\}(.*?))?\{\:\/IF\}/s";
-        if (preg_match_all($pattern, $this->content, $matches)) {
-            // var_dump($matches);
-            for ($i = 0; $i < count($matches[0]); $i++) {
-                $replace = $matches[5][$i] ?? "";
-
-                switch ($matches[2][$i]) {
-                    case "=":
-                        $replace = ($matches[1][$i] == $matches[3][$i]) ? $matches[4][$i] : $replace;
-                        break;
-                    case ">":
-                        $replace = ($matches[1][$i] > $matches[3][$i]) ? $matches[4][$i] : $replace;
-                        break;
-                    case "<":
-                        $replace = ($matches[1][$i] < $matches[3][$i]) ? $matches[4][$i] : $replace;
-                        break;
-                    case "!": {
-                            $replace = ($matches[1][$i] !== $matches[3][$i]) ? $matches[4][$i] : $replace;
-                            break;
-                        }
-                }
-                $this->content = str_replace($matches[0][$i], $replace, $this->content);
-            }
-        }
-    }
     private function replaceLoop(string $key, array $array, bool $UTF8Encode = false)
     {
         $pattern = "\\{LOOP:$key\\}(.*?)\\{\\/LOOP\\}";
         if (preg_match_all("/$pattern/is", $this->content, $matches)) {
-            $content = array_reduce($array, function ($html, $lines) use ($matches, $UTF8Encode) {
+            $index=0;
+            $content = array_reduce($array, function ($html, $lines) use ($matches, $UTF8Encode,&$index) {
                 $html .= $matches[1][0];
+                $html = str_replace("{#index#}", $index, $html); 
                 foreach ($lines as $key => $value) {
+                    
                     if (gettype($value) != "array")
                         if ($UTF8Encode)
                             $html = str_replace("{#$key#}", mb_convert_encoding($value, "UTF-8"), $html);
                         else
                             $html = str_replace("{#$key#}", $value, $html);
+                            
+                    
                 }
+                $index++;
                 return $html;
+                
             }, "");
             $this->content = str_replace($matches[0][0], $content, $this->content);
         }
     }
+    private function replaceGetVariable(){
+        foreach($_GET as $key=>$value){
+            $this->content = str_replace("{:GET name=$key}", htmlspecialchars($value), $this->content);
+        }
+    }
     private function replaceVariables()
     {
-        // var_dump($this->variables);
         foreach ($this->variables as $key => $value) {
-
-            $arr = gettype($value);
-            if ($arr != "array")
-                $this->content = str_replace("{var:$key}", html_entity_decode($value), $this->content);
-            else {
-                foreach ($value as $sKey => $sValue) {
-                    if (gettype($sValue) == "string")
-                        $this->content = str_replace("{var:$key.$sKey}", html_entity_decode(htmlentities($sValue)), $this->content);
+            $arr=gettype($value);
+            if($arr=="string")
+                $this->content = str_replace("{var:$key}", htmlspecialchars($value), $this->content);
+            else{
+                foreach($value as $sKey=>$sValue){
+                    if(gettype($sValue)=="string")
+                        $this->content = str_replace("{var:$key.$sKey}", htmlspecialchars($sValue), $this->content);
                 }
             }
         }
@@ -179,11 +146,10 @@ class EasyTemplate
     {
         if (isset($_SESSION)) {
             foreach ($_SESSION as $context => $values) {
-                if (gettype($values) == "array")
-                    foreach ($values as $name => $value) {
-                        $this->content = str_replace("{:SESSION context=\"$context\" name=\"$name\"}", htmlspecialchars($value), $this->content);
-                        $this->content = str_replace("{:SESSION name=\"$name\" context=\"$context\"}", htmlspecialchars($value), $this->content);
-                    }
+                foreach ($values as $name => $value) {
+                    $this->content = str_replace("{:SESSION context=\"$context\" name=\"$name\"}", htmlspecialchars($value), $this->content);
+                    $this->content = str_replace("{:SESSION name=\"$name\" context=\"$context\"}", htmlspecialchars($value), $this->content);
+                }
             }
         }
     }
